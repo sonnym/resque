8cf986b7e43d3b4bf80705081fec985fdcd08460
sonnym <michaud.sonny@gmail.com>
2013-10-19 17:47:39 -0400

    do not eager load

--- lib/resque/tasks.rb
+++ lib/resque/tasks.rb
@@ -58,7 +58,7 @@ namespace :resque do
   task :preload => :setup do
     if defined?(Rails) && Rails.respond_to?(:application)
       # Rails 3
-      Rails.application.eager_load!
+      # Rails.application.eager_load!
     elsif defined?(Rails::Initializer)
       # Rails 2.3
       $rails_rake_task = false
